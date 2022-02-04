e28bf629005806f55b552a1fc739b2e0b54d0c47
VinsWorldcom <vin@vinsworld.com>
2022-02-04 16:00:15 -0500

    bump version

--- resource.h
+++ resource.h
@@ -8,7 +8,7 @@
 #define VER_MAJOR   0
 #define VER_MINOR   2
 #define VER_RELEASE 5
-#define VER_BUILD   1
+#define VER_BUILD   2 
 #define VER_STRING  STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_RELEASE) "." STR(VER_BUILD)
 
 #define FILE_DESCRIPTION  "Notepad++ Plugin providing changed line highlighting."
